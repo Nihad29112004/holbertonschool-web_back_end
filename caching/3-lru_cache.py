bfa3dcf5a2b7e90c02f9716291aa02700a1c9dd6
Nihad ALiyev <lnihad257@gmail.com>
2025-10-25 22:19:04 +0400

    caching

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -1,48 +1,49 @@
 #!/usr/bin/python3
-''' LRU Caching: Create a class LRUCache that inherits from BaseCaching
-                 and is a caching system
-'''
+""" LIFO Caching: Create a class LIFOCache that inherits from BaseCaching
+                  and is a caching system
+"""
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class LRUCache(BaseCaching):
-    ''' An LRU Cache.
-        Inherits all behaviors from BaseCaching except, upon any attempt to
-        add an entry to the cache when it is at max capacity (as specified by
-        BaseCaching.MAX_ITEMS), it discards the least recently used entry to
-        accommodate for the new one.
-        Attributes:
-          __init__ - method that initializes class instance
-          put - method that adds a key/value pair to cache
-          get - method that retrieves a key/value pair from cache '''
+class LIFOCache(BaseCaching):
+    """ A LIFO Cache.
+        Inherits from BaseCaching and discards the most recently added
+        item when the cache reaches MAX_ITEMS.
+    """
 
     def __init__(self):
-        ''' Initialize class instance. '''
+        """ Initialize class instance. """
         super().__init__()
         self.keys = []
 
     def put(self, key, item):
-        ''' Add key/value pair to cache data.
-            If cache is at max capacity (specified by BaseCaching.MAX_ITEMS),
-            discard least recently used entry in cache to accommodate new
-            entry. '''
-        if key is not None and item is not None:
+        """ Add key/value pair to cache data.
+        If cache is full, discard the most recently added item (LIFO rule).
+        """
+        if key is None or item is None:
+            return
+
+        # If key already exists, update it and move to end
+        if key in self.cache_data:
             self.cache_data[key] = item
-            if key not in self.keys:
-                self.keys.append(key)
-            else:
-                self.keys.append(self.keys.pop(self.keys.index(key)))
-            if len(self.keys) > BaseCaching.MAX_ITEMS:
-                discard = self.keys.pop(0)
-                del self.cache_data[discard]
-                print('DISCARD: {:s}'.format(discard))
+            self.keys.remove(key)
+            self.keys.append(key)
+            return
+
+        # If cache full, remove the last added key (LIFO)
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discard = self.keys.pop(-1)
+            del self.cache_data[discard]
+            print(f"DISCARD: {discard}")
+
+        # Add new key
+        self.cache_data[key] = item
+        self.keys.append(key)
 
     def get(self, key):
-        ''' Return value stored in `key` key of cache.
-            If key is None or does not exist in cache, return None. '''
-        if key is not None and key in self.cache_data:
-            self.keys.append(self.keys.pop(self.keys.index(key)))
-            return self.cache_data[key]
-        return None
-        
+        """ Return value stored in `key` of cache.
+        If key is None or does not exist, return None.
+        """
+        return self.cache_data.get(key, None)
+
--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -1,48 +1,52 @@
 #!/usr/bin/python3
-''' MRU Caching: Create a class MRUCache that inherits from BaseCaching
+""" MRU Caching: Create a class MRUCache that inherits from BaseCaching
                  and is a caching system
-'''
+"""
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class MRUCache(BaseCaching):
-    ''' An MRU Cache.
-        Inherits all behaviors from BaseCaching except, upon any attempt to
-        add an entry to the cache when it is at max capacity (as specified by
-        BaseCaching.MAX_ITEMS), it discards the most recently used entry to
-        accommodate for the new one.
-        Attributes:
-          __init__ - method that initializes class instance
-          put - method that adds a key/value pair to cache
-          get - method that retrieves a key/value pair from cache '''
+    """ MRU (Most Recently Used) Cache system. """
 
     def __init__(self):
-        ''' Initialize class instance. '''
+        """ Initialize the cache. """
         super().__init__()
         self.keys = []
 
     def put(self, key, item):
-        ''' Add key/value pair to cache data.
-            If cache is at max capacity (specified by BaseCaching.MAX_ITEMS),
-            discard most recently used entry in cache to accommodate new
-            entry. '''
-        if key is not None and item is not None:
+        """ Add key/value pair to cache data.
+        If cache is full, discard the most recently used entry (MRU rule).
+        """
+        if key is None or item is None:
+            return
+
+        # If key already exists, update its value and mark as recently used
+        if key in self.cache_data:
             self.cache_data[key] = item
-            if key not in self.keys:
-                self.keys.append(key)
-            else:
-                self.keys.append(self.keys.pop(self.keys.index(key)))
-            if len(self.keys) > BaseCaching.MAX_ITEMS:
-                discard = self.keys.pop(-2)
-                del self.cache_data[discard]
-                print('DISCARD: {:s}'.format(discard))
+            self.keys.remove(key)
+            self.keys.append(key)
+            return
+
+        # If cache is full, remove the most recently used (last in list)
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discard = self.keys.pop(-1)
+            del self.cache_data[discard]
+            print(f"DISCARD: {discard}")
+
+        # Add new item
+        self.cache_data[key] = item
+        self.keys.append(key)
 
     def get(self, key):
-        ''' Return value stored in `key` key of cache.
-            If key is None or does not exist in cache, return None. '''
-        if key is not None and key in self.cache_data:
-            self.keys.append(self.keys.pop(self.keys.index(key)))
-            return self.cache_data[key]
-        return None
-        
+        """ Return the value stored in key.
+        If key exists, mark it as recently used.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+
+        # Mark as recently used
+        self.keys.remove(key)
+        self.keys.append(key)
+        return self.cache_data[key]
+
