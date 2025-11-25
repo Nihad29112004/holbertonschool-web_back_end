19bd4424da6f7caab557f55d60af278934b331e6
Nihad ALiyev <lnihad257@gmail.com>
2025-11-25 20:23:21 +0400

    fixed app.py and index.py file

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
-""" Index routes for API """
+"""
+Index routes for the API
+"""
+
 from flask import jsonify, abort
 from api.v1.views import app_views
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """ Status route """
+    """Status route"""
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized():
-    """ Route to test 401 error """
+def unauthorized_route():
+    """Route to test 401 Unauthorized error"""
     abort(401)
 
 
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden_route():
-    """ Route to test 403 error """
+    """Route to test 403 Forbidden error"""
     abort(403)
