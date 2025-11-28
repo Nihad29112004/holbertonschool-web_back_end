49fd59a31f01de9d030c54259141649b8d685019
Nihad ALiyev <lnihad257@gmail.com>
2025-11-28 23:11:44 +0400

    session

--- Session_authentication/models/user.py
+++ Session_authentication/models/user.py
@@ -1,56 +1,18 @@
 #!/usr/bin/env python3
-""" User model """
-import uuid
+"""
+User model
+"""
+
+from models.base_model import BaseModel
 
 
-class User:
-    """User model class.
-    Represents a system user with authentication attributes
-    such as email, password, first name, and last name.
+class User(BaseModel):
+    """
+    User class that inherits from BaseModel.
+    Represents a system user with authentication attributes.
     """
 
-    __users = []
-
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.email = ""
-        self.password = ""
-        self.first_name = None
-        self.last_name = None
-
-    def save(self):
-        User.__users.append(self)
-
-    @classmethod
-    def all(cls):
-        return cls.__users
-
-    @classmethod
-    def get(cls, user_id):
-        for u in cls.__users:
-            if u.id == user_id:
-                return u
-        return None
-
-    @classmethod
-    def search(cls, query):
-        results = []
-        for u in cls.__users:
-            match = True
-            for k, v in query.items():
-                if getattr(u, k, None) != v:
-                    match = False
-            if match:
-                results.append(u)
-        return results
-
-    def is_valid_password(self, pwd):
-        return self.password == pwd
-
-    def to_json(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            "first_name": self.first_name,
-            "last_name": self.last_name
-        }
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
