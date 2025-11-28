b56cf98015937abea55f0be3b0451d25a811381a
Nihad ALiyev <lnihad257@gmail.com>
2025-11-28 23:08:30 +0400

    session

--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-""" Users views """
+"""Users view module for API.
+Defines routes for user operations.
+"""
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models.user import User
@@ -7,17 +9,17 @@ from models.user import User
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def get_user(user_id):
-    """ GET /api/v1/users/<user_id> """
-
-    # 🔥 Eğer URL: /api/v1/users/me ise
+    """Retrieve a User object.
+    Args:
+        user_id (str): ID of the user to retrieve. Can be "me".
+    Returns:
+        JSON representation of the user.
+    """
     if user_id == "me":
-        # Kullanıcı doğrulanmamışsa → 404
         if request.current_user is None:
             abort(404)
-        # Doğrulanmışsa kendi profilini döndür
         return jsonify(request.current_user.to_json())
 
-    # Normal user id ile arama
     user = User.get(user_id)
     if user is None:
         abort(404)
