605850a4f1b211343920a2f26263f4347d51b95f
Nihad ALiyev <lnihad257@gmail.com>
2025-11-28 23:54:37 +0400

    session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,17 +3,13 @@
 
 import uuid
 from typing import TypeVar
+from os import getenv
 from api.v1.auth.auth import Auth
 from models.user import User
 
 
 class SessionAuth(Auth):
-    """
-    Session authentication class.
-
-    Inherits from Auth.
-    Manages sessions with in-memory storage.
-    """
+    """Session authentication class."""
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
@@ -41,10 +37,17 @@ class SessionAuth(Auth):
         """Return a User instance based on session cookie."""
         if request is None:
             return None
+
+        # Get session ID from cookie
         session_id = self.session_cookie(request)
         if session_id is None:
             return None
+
+        # Get user ID from session ID
         user_id = self.user_id_for_session_id(session_id)
         if user_id is None:
             return None
-        return User.get(user_id)
+
+        # Retrieve User object from database
+        user = User.get(user_id)
+        return user
