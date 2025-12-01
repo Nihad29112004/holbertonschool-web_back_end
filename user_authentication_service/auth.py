08335f7d6af8c5f40678825b2d55839e9071ae0d
Nihad ALiyev <lnihad257@gmail.com>
2025-12-01 20:20:32 +0400

    user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -24,12 +24,20 @@ class Auth:
     def register_user(self, email: str, password: str) -> User:
         """Register a new user with email and password"""
         try:
-            # Check if user already exists
             self._db.find_user_by(email=email)
-            # If no exception → user exists
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            # User does not exist, proceed to create
             hashed_pwd = _hash_password(password)
             user = self._db.add_user(email, hashed_pwd)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate user credentials"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+
+        if bcrypt.checkpw(password.encode("utf-8"), user.hashed_password):
+            return True
+        return False
---
+++ user_authentication_service/mian8.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from auth import Auth
+
+email = 'bob@bob.com'
+password = 'MyPwdOfBob'
+auth = Auth()
+
+auth.register_user(email, password)
+
+print(auth.valid_login(email, password))       # True
+print(auth.valid_login(email, "WrongPwd"))     # False
+print(auth.valid_login("unknown@email", password))  # False
