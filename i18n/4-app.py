0b9814be82d64bcff501e0e2e227b9b94d511dcb
Nihad ALiyev <lnihad257@gmail.com>
2025-12-18 18:12:42 +0400

    i18n

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
-""" Force locale with URL parameter """
-from flask import Flask, render_template, request
-from flask_babel import Babel, _
+"""
+Force locale with URL parameter using Flask-Babel.
 
+This module allows the user to force a locale via the URL
+parameter ?locale=[en|fr]. It uses Flask-Babel for translations
+and demonstrates the use of the _() gettext function.
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _  # _() used for translations
 
 class Config(object):
+    """
+    Configuration for Flask-Babel.
+
+    Attributes:
+        LANGUAGES (list): Supported languages.
+        BABEL_DEFAULT_LOCALE (str): Default locale.
+        BABEL_DEFAULT_TIMEZONE (str): Default timezone.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -20,6 +34,9 @@ def get_locale():
 
     Checks URL parameter ?locale=fr|en first.
     If not present or unsupported, returns best match from Accept-Language header.
+
+    Returns:
+        str: Locale code ('en' or 'fr').
     """
     locale = request.args.get("locale")
     if locale and locale in app.config["LANGUAGES"]:
@@ -27,14 +44,21 @@ def get_locale():
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-# Checker ve template uyumlu: locale_selector + Jinja global
+# Initialize Babel with locale_selector
 babel = Babel(app, locale_selector=get_locale)
-app.jinja_env.globals['get_locale'] = get_locale  # 🌟 template içinde kullanılabilir
+app.jinja_env.globals['get_locale'] = get_locale  # template use
 
 
 @app.route("/", strict_slashes=False)
 def home():
-    """Render home page with localized messages"""
+    """
+    Render the home page with localized messages.
+
+    Uses the _() function to translate strings in the template.
+
+    Returns:
+        str: Rendered HTML page.
+    """
     return render_template("4-index.html")
 
 
