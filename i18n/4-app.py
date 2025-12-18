4430eb6685e0a3f77202566b1d99c6f7e488d8d6
Nihad ALiyev <lnihad257@gmail.com>
2025-12-18 18:11:22 +0400

    i18n

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -6,28 +6,36 @@ from flask_babel import Babel, _
 
 class Config(object):
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder="templates")
 app.config.from_object(Config)
 
 
 def get_locale():
-    """Return locale from URL parameter or browser default"""
-    locale = request.args.get('locale')
-    if locale in app.config['LANGUAGES']:
+    """
+    Determine the locale to use.
+
+    Checks URL parameter ?locale=fr|en first.
+    If not present or unsupported, returns best match from Accept-Language header.
+    """
+    locale = request.args.get("locale")
+    if locale and locale in app.config["LANGUAGES"]:
         return locale
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
+# Checker ve template uyumlu: locale_selector + Jinja global
 babel = Babel(app, locale_selector=get_locale)
+app.jinja_env.globals['get_locale'] = get_locale  # 🌟 template içinde kullanılabilir
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def home():
-    return render_template('4-index.html')
+    """Render home page with localized messages"""
+    return render_template("4-index.html")
 
 
 if __name__ == "__main__":
