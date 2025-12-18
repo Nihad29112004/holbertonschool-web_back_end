847481cfae444b6c2d1f8e69493d1396cd395e86
Nihad ALiyev <lnihad257@gmail.com>
2025-12-18 17:47:17 +0400

    i18n

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -5,10 +5,10 @@ from flask_babel import Babel
 
 
 class Config(object):
-    """ Configuration Babel """
+    """ Configuration for Babel """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
@@ -16,16 +16,16 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_world():
     """ Greeting
 
         Return:
             Initial template html
     """
-    return render_template('1-index.html')
+    return render_template("1-index.html")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port=5000)
 
